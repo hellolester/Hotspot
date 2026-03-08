f89343b59391b6a2b91521193844702dfa405fd1
hellolester <bagnol.johnlester.p@gmail.com>
2026-03-08 10:15:27 +0800

    first commit

---
+++ conf.js
@@ -0,0 +1,15 @@
+var config = {
+
+loginvc : "Enter the Voucher Code then click Connect.",
+loginup : "Enter Username and Password <br> then click Connect.",
+voucherCode : "Voucher Code",
+setCase : "none", // lowercase, uppercase or none
+defaultMode : "voucher", // voucher or member
+theme : "default", // default, dark, lite
+
+// status expire
+
+url : "https://demo.mikhmon.online", // Mikhmon server URL
+SessionName : "demo",
+
+}
